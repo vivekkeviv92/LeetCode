f339def4a3ed72a85bd9a06860e667d0cf671783
Vivek Keviv <vivekkeviv92@gmail.com>
2020-04-16 01:50:52 +1000

    LeetCode_02

---
+++ src/main/java/com/coding/practice/util/ListNode.java
@@ -0,0 +1,11 @@
+package com.coding.practice.util;
+
+public class ListNode {
+    // Definition for singly-linked list.
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+}
